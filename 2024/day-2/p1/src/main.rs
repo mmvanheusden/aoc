d978d5beb742a72df78acc461caa1116f35fd9f1
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-12-07 16:56:29 +0100

    refactor: improve 2024 day 2 part 1

--- 2024/day-2/p1/src/main.rs
+++ 2024/day-2/p1/src/main.rs
@@ -1,69 +1,52 @@
-use std::collections::HashMap;
-
 fn main() {
     let input = include_str!("../../../input.txt");
     println!("{}", solve_input(input));
 }
 
 fn solve_input(input: &str) -> usize {
-    // put each character of each line into vector
-    let list = vectorize_input(input);
-    // println!("{:?}", list);
+    let mut reports: Vec<Report> = vec![];
+    for line in input.lines() {
+        reports.push(Report::from_input(line))
+    }
 
-    let mapped_values = map_safety(list);
-    println!("{:?}", mapped_values);
-
-    // count results
-
-    let mut amount = 0;
-
-    amount = mapped_values.iter().filter(|(k,v)| **v == true).count();
-
-    amount
+    reports.retain(|r| r.has_valid_steps() && r.is_increasing_or_decreasing());
+    reports.iter().count()
 }
 
-#[derive(Debug)]
-enum ArrayOrder {
-    Increasing,
-    Decreasing,
+#[derive(Debug, Eq, PartialEq)]
+struct Report(
+    Vec<u8>
+);
+
+impl Report {
+    fn from_input(line: &str) -> Report {
+        Report(line.split_whitespace().map(|x| x.parse::<u8>().unwrap()).collect())
+    }
+
+    fn is_increasing_or_decreasing(&self) -> bool {
+        check_order(&self.0)
+    }
+
+    fn has_valid_steps(&self) -> bool {
+        valid_steps(&self.0)
+    }
 }
 
-fn vectorize_input(input: &str) -> Vec<Vec<u8>> {
+
+fn convert_input(input: &str) -> Vec<Vec<u8>> {
     let mut result = Vec::new();
     for line in input.lines() {
-        // println!("{}", line);
         result.push(line.split_whitespace().map(|x| x.parse::<u8>().unwrap()).collect())
     }
 
     result
 }
 
-fn map_safety(array: Vec<Vec<u8>>) -> HashMap<Vec<u8>, bool> {
-    let mut hashmap = HashMap::new();
-    for item in array {
-        let mut safety = true;
-        if check_order(item.clone()).is_none() {
-            safety = false;
-        }
-
-        if !valid_steps(item.clone()) {
-            safety = false;
-        }
-
-        hashmap.insert(
-            item.clone(),
-            safety,
-        );
-    }
-
-    hashmap
-}
-
-fn check_order(array: Vec<u8>) -> Option<ArrayOrder> {
+fn check_order(array: &Vec<u8>) -> bool {
     let mut increasing = true;
     let mut decreasing = true;
     if array.len() <= 1 {
-        return None;
+        return true;
     }
 
     for i in 1..array.len() {
@@ -76,28 +59,20 @@ fn check_order(array: Vec<u8>) -> Option<ArrayOrder> {
         }
     }
 
-    if increasing {
-        Some(ArrayOrder::Increasing)
-    } else if decreasing {
-        Some(ArrayOrder::Decreasing)
-    } else {
-        None
-    }
+    increasing || decreasing
 }
 
-fn valid_steps(array: Vec<u8>) -> bool {
+fn valid_steps(array: &Vec<u8>) -> bool {
     if array.len() == 1 {
-        return (usize::abs_diff(array[0] as usize, array[1] as usize) == 1)
+        return array[0].abs_diff(array[1]) == 1
     }
 
     for i in 1..array.len() {
-        let eval = (1..4).contains(&usize::abs_diff(array[i] as usize, array[i - 1] as usize));
+        let eval = (1..4).contains(&array[i].abs_diff(array[i - 1]));
         if !eval {
             return false;
         }
     }
-
-    // println!("{:?} has right step", array);
 
     true
 }
