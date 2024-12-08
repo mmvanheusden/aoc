52e071374919b4d22dff19ac4724ea545383cc64
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-12-08 17:04:23 +0100

    refactor: cleanup & comment 2024 day 3 part 1

--- 2024/day-3/d3-p1/src/main.rs
+++ 2024/day-3/d3-p1/src/main.rs
@@ -6,40 +6,30 @@ fn main() {
     println!("{}", solve_input(input));
 }
 
-fn solve_input(input: &str) -> &str {
+fn solve_input(input: &str) -> u128 {
     // extract all instructions with my awesome expression
     let ex = Regex::new(r"mul\(\d*,\d*\)").unwrap();
-    let matches: Vec<&str> = ex.find_iter(input).map(|m| m.as_str()).collect();
-
-    // println!("{:?}", matches);
-
-
-    let mut instructions: Vec<Multiplication> = vec![];
-    for matsj in matches {
-        let mut iter = matsj.splitn(2, ',');
-        let l = iter.next().unwrap();
-        let r = iter.next().unwrap();
-
-        // println!("{}", l);
-        // println!("{}", r);
-
-        instructions.push(Multiplication {
-            a: l[4..].parse::<u128>().unwrap(),
-            b: r[..r.len()-1].parse::<u128>().unwrap(),
-        })
-    }
-
-    // println!("{:?}", instructions);
-
+    let matches: Vec<&str> = ex.find_iter(input).map(|m| m.as_str()).collect(); // walk through input and convert matches to string and push to array.
+    let instructions: Vec<Multiplication> = matches.into_iter().map(Multiplication::from_instruction).collect();
 
     let sum: u128 = instructions.iter().map(|m| m.a * m.b).sum();
 
-    println!("{}", sum);
-    "a"
+    sum
 }
 
 #[derive(Debug)]
 struct Multiplication {
     a: u128,
     b: u128
+}
+
+impl Multiplication {
+    fn from_instruction(instruction: &str) -> Multiplication {
+        let (l,r) = instruction.split_once(",").unwrap(); // unpack l and r into tuple.
+
+        Multiplication {
+            a: l[4..].parse::<u128>().unwrap(),
+            b: r[..r.len()-1].parse::<u128>().unwrap(),
+        }
+    }
 }
