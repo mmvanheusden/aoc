15ddba4c07abaf60fb233025ee7a05eb63ee7046
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-12-01 10:21:48 +0100

    feat(2025-d1-p1): create enum with a rotation

--- 2025/day-1/p1/src/main.rs
+++ 2025/day-1/p1/src/main.rs
@@ -4,5 +4,24 @@ fn main() {
 }
 
 fn solve_input(input: &str) -> &str {
+    for line in input.lines() {
+        // if line == "\n" { continue }
+        let rotation: Rotation = match line.chars().next().unwrap() {
+            'R' => {
+                Rotation::Left(line[1..line.len()].parse::<u32>().unwrap())
+            },
+            'L' => {
+                Rotation::Right(line[1..line.len()].parse::<u32>().unwrap())
+            },
+            _ => panic!()
+        };
+        dbg!(rotation);
+    }
     return "a"
+}
+
+#[derive(Debug)]
+enum Rotation {
+    Left(u32),
+    Right(u32)
 }
