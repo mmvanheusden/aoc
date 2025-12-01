edee40b460771b1120354632200915d03bff2296
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-12-01 09:46:33 +0100

    chore: update template

--- 2025/day-1/p1/src/main.rs
+++ 2025/day-1/p1/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    println!("{}", solve_input(input));
+}
+
+fn solve_input(input: &str) -> &str {
+    return "a"
 }
