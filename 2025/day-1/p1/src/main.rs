f2d31e7aafdf25e0891b5227c0137fecbd202154
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-12-02 15:12:51 +0100

    chore(2025-d1-p1): solve

--- 2025/day-1/p1/src/main.rs
+++ 2025/day-1/p1/src/main.rs
@@ -3,25 +3,55 @@ fn main() {
     println!("{}", solve_input(input));
 }
 
-fn solve_input(input: &str) -> &str {
-    for line in input.lines() {
-        // if line == "\n" { continue }
-        let rotation: Rotation = match line.chars().next().unwrap() {
-            'R' => {
-                Rotation::Left(line[1..line.len()].parse::<u32>().unwrap())
+fn solve_input(input: &str) -> String {
+    let mut DIAL = 50;
+    let mut hit_zero = 0;
+    let rotations = parse_to_rotations(input);
+    rotations.iter().for_each(|rotation| {
+        println!("\nDIAL before: {DIAL}\tI: {:?}", rotation);
+        match rotation {
+            Rotation::Left(x) => {
+                let clicks = x % 100; // Hiermee zorgen we dat we nooit meer dan 1 rondje draaien.
+                if clicks > DIAL {
+                    DIAL += 100 - clicks;
+                } else {
+                    DIAL -= clicks;
+                }
             },
+            Rotation::Right(x) => {
+                let clicks = x % 100;
+                DIAL += clicks;
+                if DIAL >= 100 {
+                    DIAL -= 100;
+                }
+            }
+        };
+        if DIAL == 0 {
+            hit_zero += 1;
+        };
+        println!("DIAL after: {DIAL}");
+    });
+    return format!("HZ: {hit_zero}")
+}
+
+fn parse_to_rotations(input: &str) -> Vec<Rotation> {
+    let mut rotations: Vec<Rotation>  = vec![];
+    for line in input.lines() {
+       rotations.push(match line.chars().next().unwrap() {
             'L' => {
-                Rotation::Right(line[1..line.len()].parse::<u32>().unwrap())
+                Rotation::Left(line[1..line.len()].parse::<i32>().unwrap())
+            },
+            'R' => {
+                Rotation::Right(line[1..line.len()].parse::<i32>().unwrap())
             },
             _ => panic!()
-        };
-        dbg!(rotation);
+        });
     }
-    return "a"
+    return rotations
 }
 
 #[derive(Debug)]
 enum Rotation {
-    Left(u32),
-    Right(u32)
+    Left(i32),
+    Right(i32)
 }
