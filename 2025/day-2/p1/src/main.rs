d3a8b689777cec05543ad167be2189d99ce80264
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-12-02 16:17:38 +0100

    feat(2025-d1-p1): parse input to inclusive ranges

--- 2025/day-2/p1/src/main.rs
+++ 2025/day-2/p1/src/main.rs
@@ -1,8 +1,21 @@
+use std::ops::{RangeInclusive};
+
 fn main() {
     let input = include_str!("../input.txt");
     println!("{}", solve_input(input));
 }
 
 fn solve_input(input: &str) -> &str {
+    dbg!(input_to_ranges(input));
     return "a"
+}
+
+fn input_to_ranges(input: &str) -> Vec<RangeInclusive<i64>> {
+    let mut ranges: Vec<RangeInclusive<i64>> = vec![];
+    let blocks: Vec<&str> = input.split(',').collect();
+    blocks.iter().for_each(|block| {
+        let split = block.split('-').collect::<Vec<&str>>();
+        ranges.push(RangeInclusive::new(split[0].parse::<i64>().unwrap(), split[1].parse::<i64>().unwrap()));
+    });
+    return ranges
 }
